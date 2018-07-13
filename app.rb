3c3a5034d973f1fd44ab593f382652a811d26c35
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 20:11:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require 'sinatra/base'
 # require_relative 'config/environment'
 class App < Sinatra::Base
-  get "/" do
-
-  end
-  # get '/newteam' do
-  #   erb :newteam
-  # end
+  # get "/" do
   #
-  # post '/newteam' do
-  #   erb :team
   # end
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/newteam' do
+    erb :team
+  end
 end
